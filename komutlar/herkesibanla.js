313bbfbd82bc32970096004fe2eff8ada97c7186
BenimAdimName123 <45329977+BenimAdimName123@users.noreply.github.com>
2018-11-26 19:19:47 +0300

    Add files via upload

---
+++ komutlar/herkesibanla.js
@@ -0,0 +1,26 @@
+﻿const Discord = require('discord.js');
+
+exports.run = (client, message, args) => {
+	let mesaj = args.slice(0).join(' ');
+	if (mesaj.length < 1) return message.reply('**Kimi Banlanayacağımı Yazmalısın**');
+    const embed = new Discord.RichEmbed()
+    .setAuthor('')
+    .setColor('RANDOM')
+    .setDescription(`** ${mesaj} ` + message.author.username + ' Adlı Kullanıcı Sizi Banladı :hammer: (Gerçek DEĞİLDİR BU BİR EĞLENCE 
+KOMUTUDUR)**')
+        .setImage(`https://media.giphy.com/media/fe4dDMD2cAU5RfEaCU/giphy.gif`)
+    return message.channel.sendEmbed(embed);
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'banla',
+  description: 'Seçtiğiniz kullanıcıyı banlarsınız (gerçek değil).',
+  usage: 'banla'
+};
